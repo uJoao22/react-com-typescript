88af31184d27f5875b0a681ea2ca52299d7a8a4d
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-06-30 22:29:12 -0300

    Primeiros passos e primeiro componente

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+function Banner() {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" alt="O banner principal dá página do Organo"></img>
+        </header>
+    )
+}
+
+export default Banner;
