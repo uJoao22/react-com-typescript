a0051c9a217b8e63128c2c524b5a569f24eeaa9b
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-07-05 21:49:56 -0300

    Ajuste formulario

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,10 +5,10 @@ import DropDown from '../DropDown';
 import Button from '../Button';
 
 export const Form = (props) => {
-    const [nome, setNome] = useState('João');
-    const [cargo, setCargo] = useState('Programador Junior');
-    const [imagem, setImagem] = useState('https://github.com/uJoao22.png');
-    const [time, setTime] = useState('Programação');
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+    const [time, setTime] = useState('');
     
     const save = (e) => {
         e.preventDefault();
